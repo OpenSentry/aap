6fbf9c8a2100dc8544ac6b97b3a4e8f99d10220f
Lasse Nielsen <65roed@gmail.com>
2019-06-17 10:58:53 +0200

    Added simple ping pong example for cp-be

---
+++ controller/v1/authorizations.go
@@ -0,0 +1,25 @@
+package controller
+
+import (
+  "github.com/gin-gonic/gin"
+  "net/http"
+  _ "golang-cp-be/gateway/db"
+)
+
+func GetAuthorizations(c *gin.Context) {
+  c.JSON(http.StatusOK, gin.H{
+    "message": "pong",
+  })
+}
+
+func PostAuthorizations(c *gin.Context) {
+  c.JSON(http.StatusOK, gin.H{
+    "message": "pong",
+  })
+}
+
+func PutAuthorizations(c *gin.Context) {
+  c.JSON(http.StatusOK, gin.H{
+    "message": "pong",
+  })
+}
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "github.com/gin-gonic/gin"
+  "golang-cp-be/server"
+)
+
+/*
+
+/authorizations
+
+*/
+
+func main() {
+
+  r := gin.Default()
+  //r.GET("/ping", controller.Ping)
+
+  v1 := r.Group("v1")
+
+  server.V1Routes(v1) //Added all routes
+
+  r.Run() // listen and serve on 0.0.0.0:8080
+}
