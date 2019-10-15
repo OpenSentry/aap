f00e5382b36f72eef3e37913dca59041191399c2
Lasse Nielsen <charmixer@users.noreply.github.com>
2019-10-15 16:13:59 +0200

    publish endpoint read

--- client/publishes.go
+++ client/publishes.go
@@ -9,6 +9,7 @@ import (
 type Publish struct {
   Publisher         string    `json:"publisher_id" validate:"required,uuid"`
   Scope             string    `json:"scope" validate:"required"`
+  MayGrantScope     string    `json:"may_grant_scope" validate:"omitempty"`
   Title             string    `json:"title" validate:"required"`
   Description       string    `json:"description"`
 }
