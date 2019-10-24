eafdda462a30f67ab62e6c6be610cfde4935d643
Marc <wraix@users.noreply.github.com>
2019-10-24 23:26:17 +0200

    Simple version of judge

---
+++ endpoints/entities/judge.go
@@ -0,0 +1,113 @@
+package entities
+
+import (
+  "net/http"
+  "github.com/sirupsen/logrus"
+  "github.com/gin-gonic/gin"
+
+  "github.com/charmixer/aap/environment"
+  "github.com/charmixer/aap/gateway/aap"
+  "github.com/charmixer/aap/client"
+
+  bulky "github.com/charmixer/bulky/server"
+)
+
+func GetEntitiesJudge(env *environment.State) gin.HandlerFunc {
+  fn := func(c *gin.Context) {
+    log := c.MustGet(environment.LogKey).(*logrus.Entry)
+    log = log.WithFields(logrus.Fields{
+      "func": "GetEntitiesJudge",
+    })
+
+    var requests []client.ReadEntitiesJudgeRequest
+    err := c.BindJSON(&requests)
+    if err != nil {
+      c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+      return
+    }
+
+    var handleRequests = func(iRequests []*bulky.Request) {
+
+      session, tx, err := aap.BeginReadTx(env.Driver)
+      if err != nil {
+        bulky.FailAllRequestsWithInternalErrorResponse(iRequests)
+        log.Debug(err.Error())
+        return
+      }
+      defer tx.Close() // rolls back if not already committed/rolled back
+      defer session.Close()
+
+      requestor := c.MustGet("sub").(string)
+      iRequestor := aap.Identity{Id:requestor}
+
+      for _, request := range iRequests {
+        r := request.Input.(client.ReadEntitiesJudgeRequest)
+
+        iPublisher := aap.Identity{Id:r.Publisher}
+
+        grantedOwnersCount := 0
+
+        for _, id := range r.Owners {
+          iOwner := aap.Identity{Id:id}
+
+          var grantedScopes []aap.Scope
+          var missingScopes []aap.Scope
+
+          for _, scope := range r.Scopes {
+            iScope := aap.Scope{Name:scope}
+
+            verdict, err := aap.JudgeEntity(tx, iPublisher, iRequestor, iOwner, iScope)
+            if err != nil {
+              e := tx.Rollback()
+              if e != nil {
+                log.Debug(e.Error())
+              }
+              bulky.FailAllRequestsWithServerOperationAbortedResponse(iRequests) // Fail all with abort
+              request.Output = bulky.NewInternalErrorResponse(request.Index)
+              log.Debug(err.Error())
+              return
+            }
+
+            if verdict.Granted == true {
+              grantedScopes = append(grantedScopes, verdict.Scope)
+            } else {
+              missingScopes = append(missingScopes, verdict.Scope)
+            }
+
+          }
+
+          if len(missingScopes) == 0 && len(grantedScopes) == len(r.Scopes) {
+            // Granted
+            grantedOwnersCount += 1
+          }
+
+        }
+
+        if grantedOwnersCount == len(r.Owners) {
+          request.Output = bulky.NewOkResponse(request.Index, client.ReadEntitiesJudgeResponse{
+            Granted: true,
+          })
+          continue
+        }
+
+        // Deny by default
+        request.Output = bulky.NewOkResponse(request.Index, client.ReadEntitiesJudgeResponse{
+          Granted: false,
+        })
+      }
+
+      err = bulky.OutputValidateRequests(iRequests)
+      if err == nil {
+        tx.Commit()
+        return
+      }
+
+      // Deny by default
+      tx.Rollback()
+    }
+
+    responses := bulky.HandleRequest(requests, handleRequests, bulky.HandleRequestParams{})
+    c.JSON(http.StatusOK, responses)
+  }
+  return gin.HandlerFunc(fn)
+}
--- gateway/aap/entities.go
+++ gateway/aap/entities.go
@@ -1,10 +1,21 @@
 package aap
 
 import (
+  //"strings"
+  "fmt"
   "github.com/neo4j/neo4j-go-driver/neo4j"
 
   "github.com/charmixer/aap/config"
 )
+
+type EntityVerdict struct {
+  Publisher Identity
+  Requestor Identity
+  Owner Identity
+  Scope Scope
+
+  Granted bool
+}
 
 func CreateEntity(tx neo4j.Transaction, iEntity Identity, iCreator Identity, iRequest Identity) (rEntity Identity, err error) {
 
@@ -73,4 +84,104 @@ func CreateEntity(tx neo4j.Transaction, iEntity Identity, iCreator Identity, iRe
   rEntity = iEntity
 
   return rEntity, nil
+}
+
+// F_judge(I_rs, I_requestor, [I_owner], [S]) => [ I_owner, T/F, [S_granted], [S_missing] ] where I_requestor := [I_client, I_human]
+func JudgeEntity(tx neo4j.Transaction, iPublisher Identity, iRequestor Identity, iOwner Identity, iScope Scope) (verdict EntityVerdict, err error) {
+  var result neo4j.Result
+  var cypher string
+  var params = make(map[string]interface{})
+
+/*
+  var cypOwners string
+  var cypScopes string
+
+  if len(iScopes) > 0 {
+    var scopes []string
+    for _, scope := range iScopes {
+      scopes = append(scopes, scope.Name)
+    }
+    cypScopes = `and scope.name in split($scopes, ",")`
+    params["scopes"] = strings.Join(scopes, ",")
+  }
+
+  if len(iOwners) > 0 {
+    var owners []string
+    for _, e := range iOwners {
+      owners = append(owners, e.Id)
+    }
+    cypOwners = `and owner.id in split($owners, ",")`
+    params["owners"] = strings.Join(owners, ",")
+  }*/
+
+/*
+
+  // AAP.Id a73b547b-f26d-487b-9e3a-2574fe3403fe
+  // Marc (60e1ce36-6e2b-448c-b8e1-dfd5e8af0042) vil gerne give rettigheden login til Cactus (1cd78bcd-ccac-433d-970f-8ef6b12ecd84) til Sune
+  // Dette betyder at marc skal give scope 'openid' til Sune
+
+  // For at give scope 'openid' til Sune skal Marc have lov til at give rettigheder til Cactus
+  // Dette betyder at Marc skal være granted en regel som på vegne af Cactus giver 'openid'
+
+  // Funktionen 'at give rettigheder' bliver published af AAP og er givet ved scope 'aap:create:grants'
+
+  // Dette vil sige at følgende sti i grafen skal findes for Marc
+  MATCH (aap:Identity {id:"a73b547b-f26d-487b-9e3a-2574fe3403fe"})-[:IS_PUBLISHING]->(pr:Publish:Rule)-[:PUBLISH]->(s:Scope {name:"aap:create:grants"})
+  MATCH (i:Identity {id:"60e1ce36-6e2b-448c-b8e1-dfd5e8af0042"})-[:IS_GRANTED]->(gr:Grant:Rule)-[:GRANTS]->(pr)
+  MATCH (i)-[:IS_GRANTED]->(gr)-[:ON_BEHALF_OF]->(obo:Identity {id:"1cd78bcd-ccac-433d-970f-8ef6b12ecd84"})
+  return aap, pr, s, i, gr, obo
+
+*/
+
+  cypher = fmt.Sprintf(`
+    MATCH (publisher:Identity {id:$publisher})-[:IS_PUBLISHING]->(pr:Publish:Rule)-[:PUBLISH]->(scope:Scope {name:$scope})
+    MATCH (requestor:Identity {id:$requestor})-[:IS_GRANTED]->(gr:Grant:Rule)-[:GRANTS]->(pr)
+    MATCH (requestor)-[:IS_GRANTED]->(gr)-[:ON_BEHALF_OF]->(owner:Identity {id:$owner})
+    RETURN publisher, requestor, owner, scope, gr, pr
+  `)
+
+  params["publisher"] = iPublisher.Id
+  params["requestor"] = iRequestor.Id
+  params["owner"] = iOwner.Id
+  params["scope"] = iScope.Name
+
+  if result, err = tx.Run(cypher, params); err != nil {
+    return verdict, err
+  }
+
+  for result.Next() {
+    record          := result.Record()
+    publisherNode   := record.GetByIndex(0)
+    requestorNode   := record.GetByIndex(1)
+    ownerNode       := record.GetByIndex(2)
+    scopeNode       := record.GetByIndex(3)
+
+    if publisherNode != nil && requestorNode != nil && ownerNode != nil && scopeNode != nil {
+      p := marshalNodeToIdentity(publisherNode.(neo4j.Node))
+      r := marshalNodeToIdentity(requestorNode.(neo4j.Node))
+      o := marshalNodeToIdentity(ownerNode.(neo4j.Node))
+      s := marshalNodeToScope(scopeNode.(neo4j.Node))
+
+      verdict = EntityVerdict{
+        Publisher: p,
+        Requestor: r,
+        Owner: o,
+        Scope: s,
+        Granted: true,
+      }
+    } else {
+      verdict = EntityVerdict{
+        Granted: false,
+      }
+    }
+  }
+
+  logCypher(cypher, params)
+
+  // Check if we encountered any error during record streaming
+  if err = result.Err(); err != nil {
+    return verdict, err
+  }
+
+  return verdict, nil
 }
