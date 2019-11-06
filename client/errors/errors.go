1c5aee3a91ba8a958aa02510871f17f21fb0d999
Marc <wraix@users.noreply.github.com>
2019-11-06 14:50:20 +0100

    wip

--- client/errors/errors.go
+++ client/errors/errors.go
@@ -1,58 +1,34 @@
 package errors
 
-const DEFAULT_LANG = 1
-const DEV = 0
-const EN = 1
+import (
+  bulky "github.com/charmixer/bulky/errors"
+)
 
 const INPUT_VALIDATION_FAILED    = 1
 const EMPTY_REQUEST_NOT_ALLOWED  = 2
 const MAX_REQUESTS_EXCEEDED      = 3
 const FAILED_DUE_TO_OTHER_ERRORS = 4
 const INTERNAL_SERVER_ERROR      = 5
-const OPERATION_ABORTED          = 6
 
-const CONSENT_NOT_FOUND          = 40
+const CONSENT_NOT_FOUND = 10
+const NO_CONSENT_REQUESTS = 11
+const INVALID_SCOPES = 12
 
-// keep em' static
-var ERRORS = map[int]map[int]string{
-  INPUT_VALIDATION_FAILED:
-    {
-      EN:  "Input validation failed",
-      DEV: "Struct validations failed on tags for input",
+func InitRestErrors() {
+  bulky.AppendErrors(
+    map[int]map[string]string{
+      CONSENT_NOT_FOUND: {
+        "en": "Not found",
+        "dev": "Consent not found",
+      },
+      NO_CONSENT_REQUESTS: {
+        "en": "No consent requests",
+        "dev": "No consent requests. Hint: Missing subscription on any of the requested scopes and audiences.",
+      },
+      INVALID_SCOPES: {
+        "en": "Invalid scopes",
+        "dev": "Invalid scopes. Hint: Atleast one requested scope is not subscribed for any audience.",
+      },
     },
-  EMPTY_REQUEST_NOT_ALLOWED:
-    {
-      EN:  "Empty request not allowed",
-      DEV: "This endpoint does not allow the empty request - each request must be defined separately",
-    },
-  MAX_REQUESTS_EXCEEDED:
-    {
-      EN:  "Max number of requests exceeded",
-      DEV: "MaxRequest parameter has been set for endpoint and is exceeded by the number of request-objects given in the input",
-    },
-  FAILED_DUE_TO_OTHER_ERRORS:
-    {
-      EN:  "Failed due to other errors",
-      DEV: "Other request has already been invalidated, no reason to continue until those have been fixed",
-    },
-  INTERNAL_SERVER_ERROR:
-    {
-      EN:  "Internal server error occured. Please wait until it has been fixed, before you try again",
-      DEV: "Internal server error occured. Please wait until it has been fixed, before you try again",
-    },
-  OPERATION_ABORTED:
-    {
-      EN:  "Operation aborted due to other internal server errors",
-      DEV: "Operation aborted due to other internal server errors",
-    },
-
-
-  CONSENT_NOT_FOUND:
-    {
-      EN:  "Not found",
-      DEV: "Consent not found",
-    },
+  )
 }
-
-// E[INPUT_VALIDATION_FAILED][EN]
-// client.E[client.INPUT_VALIDATION_FAILED][client.EN]
